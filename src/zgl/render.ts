961a240de0a328e9d4586aca22f506ac7f3ae833
doug <linderd@iinet.net.au>
2014-05-07 17:05:39 +0800

    renderer now is closer to being useful

--- src/zgl/render.ts
+++ src/zgl/render.ts
@@ -16,19 +16,18 @@ module zgl {
 
     /* Types of buffers that can be bound */
     export enum BufferType {
-        PROJECTION,
-        MODELVIEW,
-        VERTEX,
-        FRAGMENT,
+        UNIFORM_MAT4,
+        VERTEX_FLOAT3,
+        VERTEX_FLOAT2
     }
 
     /* The currently held config for a buffer binding */
     export interface BufferBinding {
         attribute:string;
-        location:any;
-        shader:zgl.Shader;
         type:BufferType;
+        location:any;
         buffer:any;
+        dirty:boolean;
     }
 
     /* A renderer looks purely after assigning buffers to shaders */
@@ -37,20 +36,80 @@ module zgl {
         /* The program used by this renderer */
         program:zgl.Program;
 
+        /* Attribute binding */
+        private _bindings:{[key:string]:BufferBinding} = {};
+
+        /* Is this rendering active */
+        private _active:boolean = false;
+
         constructor(program:zgl.Program) {
             this.program = program;
         }
 
         /* Bind a buffer to an attribute for a shader */
-        public bind(gl:any):void {
+        public bind(gl:any, attribute:string, type:BufferType, buffer:any = null):void {
+            if (this._bindings[attribute]) {
+                throw new Error('Invalid attribute name "' + attribute + '" already in use');
+            }
+            this._bindings[attribute] = {
+                attribute:attribute,
+                type:type,
+                buffer:buffer,
+                location:null,
+                dirty:true
+            }
         }
 
         /* Load the buffers for this renderer */
-        public load(gl:any):void {
+        public buffer(attributes:string, buffer:any):void {
+            if (!this._bindings[attribute]) {
+                throw new Error('')
+            }
+            this._bindings[attributes].buffer = buffer;
+            this._bindings[attributes].dirty = true;
         }
 
         /* Draw this renderer */
-        public render(gl:any):void {
+        public render(gl:any, use_cache:boolean = true):void {
+            var reload = !this._active || !use_cache;
+
+            // Load the program
+            if (reload) { gl.useProgram(this.program.program); }
+
+            // Now bind each buffer to each program location
+            for (var key in this._bindings) {
+
+                // Get the attribute binding
+                var attrib = this._bindings[key];
+
+                // Get the location
+                var location = attrib.location;
+                if (reload || !location) {
+                    switch(attrib.type) {
+                        case BufferType.UNIFORM_MAT4:
+                            break;
+
+                        case BufferType.VERTEX_FLOAT2:
+                        case BufferType.VERTEX_FLOAT3:
+                            break;
+                    }
+                }
+
+                // Bind buffer
+                if (reload || attrib.dirty) {
+                    switch(attrib.type) {
+                        case BufferType.UNIFORM_MAT4:
+                            break;
+
+                        case BufferType.VERTEX_FLOAT2:
+                        case BufferType.VERTEX_FLOAT3:
+                            break;
+                    }
+                }
+            }
+
+            // Push array
+            // ...
         }
     }
 }
