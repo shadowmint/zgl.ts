1ebd5d1d581c83b317d2876ebb170058d915b62c
doug <linderd@iinet.net.au>
2014-05-20 13:42:57 +0800

    Added a demo

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,31 @@
 module.exports = function (grunt) {
 
+    // Extension glue
+    var config = {};
+    var extend = function(destination, source) { for (var property in source) { if (destination.hasOwnProperty(property)) { extend(destination[property], source[property]); } else { destination[property] = source[property]; } } return destination; };
+    var configure = function(source) { extend(config, source); };
+
+    // Tasks
     grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-exec');
     grunt.loadNpmTasks('grunt-ts');
 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+    // Common
+    configure({
         clean: {
-            lib: {
-                src: ['bin/*.js.map', 'bin/*.js']
-            }
-        },
+            src: ['dist/**/*.js.map', 'dist/**/*.js', 'dist/**/*.d.ts']
+        }
+    });
+
+    // zgl
+    configure({
         ts: {
-            xz: {
-                src: ['src/zgl/**/*.ts'],
-                out: 'bin/zgl.js',
+            zgl: {
+                src: ['src/zgl/__init__.ts'],
+                out: 'dist/zgl.js',
                 options: {
+                    module: 'amd',
                     target: 'es3',
-                    sourceMaps: false,
+                    sourceMaps: true,
                     declaration: true,
                     removeComments: false
                 }
@@ -25,6 +33,31 @@ module.exports = function (grunt) {
         }
     });
 
-    // Main build
-    grunt.registerTask('default', ['clean', 'ts:zgl']);
+    // z3d
+    configure({
+        ts: {
+            z3d: {
+                src: ['src/z3d/__init__.ts'],
+                out: 'dist/z3d.js',
+                options: {
+                    module: 'amd',
+                    target: 'es3',
+                    sourceMaps: true,
+                    declaration: true,
+                    removeComments: false
+                }
+            }
+        }
+    });
+
+    // Load combined config
+    console.log(config);
+    grunt.initConfig(config);
+
+    // Builder tasks
+    grunt.registerTask('_zgl', ['ts:zgl']);
+    grunt.registerTask('_z3d', ['ts:z3d']);
+
+    // External tasks
+    grunt.registerTask('default', ['clean', '_zgl', '_z3d']);
 }
