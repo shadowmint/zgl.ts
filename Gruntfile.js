e574da07556d93e1d0250ea004e4080ede4c8994
doug <linderd@iinet.net.au>
2014-07-07 22:58:57 +0800

    Removed browserify

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,22 +30,25 @@ module.exports = function (grunt) {
                     declaration: true,
                     comments: true
                 }
+            },
+            zgl_amd: {
+                src: ['<%= path.src %>/zgl/**/*.ts'],
+                dest: '<%= path.build %>/zgl',
+                options: {
+                    module: 'amd',
+                    target: 'es3',
+                    basePath: '<%= path.src %>',
+                    sourceMap: false,
+                    declaration: true,
+                    comments: true
+                }
             }
         },
         nodeunit: {
             zgl: [ '<%= path.build %>/zgl/**/*_tests.js' ]
-        },
-        browserify: {
-            zgl: {
-                files: {
-                    'dist/zgl.js': [ '<%= path.build %>/zgl/**/*.js' ]
-                }
-            }
-
         }
     });
-    ext.registerTask('zgl', ['typescript:zgl', 'nodeunit:zgl']);
-    ext.registerTask('zgl_build', ['zgl', 'browserify:zgl']);
+    ext.registerTask('zgl', ['typescript:zgl', 'nodeunit:zgl', 'typescript:zgl_amd']);
 
     // z3d
     ext.configure({
@@ -61,25 +64,28 @@ module.exports = function (grunt) {
                     declaration: true,
                     comments: true
                 }
+            },
+            z3d_amd: {
+                src: ['<%= path.src %>/z3d/**/*.ts'],
+                dest: '<%= path.build %>/z3d',
+                options: {
+                    module: 'amd',
+                    target: 'es3',
+                    basePath: '<%= path.src %>',
+                    sourceMap: false,
+                    declaration: true,
+                    comments: true
+                }
             }
         },
         nodeunit: {
             z3d: [ '<%= path.build %>/z3d/**/*_tests.js' ]
-        },
-        browserify: {
-            z3d: {
-                files: {
-                    'dist/z3d.js': [ '<%= path.build %>/**/*.js' ]
-                }
-            }
-
         }
     });
-    ext.registerTask('z3d', ['typescript:z3d', 'nodeunit:z3d']);
-    ext.registerTask('z3d_build', ['z3d', 'browserify:z3d']);
+    ext.registerTask('z3d', ['typescript:z3d', 'nodeunit:z3d', 'typescript:z3d_amd']);
 
     // Common build tasks
-    ext.registerTask('build', ['zgl_build', 'z3d_build'])
+    ext.registerTask('build', ['zgl', 'z3d'])
 
     // External tasks
     ext.initConfig(grunt);
